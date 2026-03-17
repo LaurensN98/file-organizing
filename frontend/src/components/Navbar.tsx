aa2d581853bca9d29c0820a415f721eac589aa8b
Laurens Niesten <laurensniesten@gmail.com>
2026-03-17 10:03:32 +0100

    feat: navbar links placement

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,9 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
+  const links = [
+    // { name: "History", href: "/history" },
+    { name: "Organize", href: "/" },
+    { name: "Pricing", href: "/#pricing" },
+    { name: "About", href: "/#about" },
+    { name: "Contact", href: "/#contact" },
+  ];
+
   return (
-    <div className="w-full h-20 sticky top-0 left-0 z-50 bg-white shadow-md flex justify-between items-center px-6 text-black">
-      <Link href="/">
+    <div className="w-full h-20 sticky top-0 left-0 z-50 bg-white shadow-md flex items-center px-6 text-black">
+      <Link href="/" className="absolute">
         <div className="w-32">
           <Image
             src="/svgs/logo.svg"
@@ -20,11 +28,15 @@ export default function Navbar() {
         </div>
       </Link>
 
-      <Link href="/history">
-        <div className="font-semibold text-[#203047] hover:text-blue-500">
-          History
-        </div>
-      </Link>
+      <div className="flex items-center gap-16 w-full justify-center">
+        {links.map((link) => (
+          <Link key={link.name} href={link.href}>
+            <div className="font-semibold text-[#203047] hover:text-[#4A80A6]">
+              {link.name}
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
