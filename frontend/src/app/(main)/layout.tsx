338770c32a4e23b703f4e8aa3e468bf8e9004be1
Laurens Niesten <laurensniesten@gmail.com>
2026-03-18 15:12:47 +0100

    feat: changed page metadata

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -4,7 +4,6 @@ import "../globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-// Configure Satoshi
 const satoshi = localFont({
   src: "../../../public/fonts/Satoshi-Variable.ttf", // Path to your font file
   variable: "--font-satoshi", // Optional: defines a CSS variable
@@ -12,15 +11,15 @@ const satoshi = localFont({
 
 export const metadata: Metadata = {
   title: "Organize Your File Automatically | Neatly",
-  description: "Privacy-focused document organization using AI",
+  description: "Organize your messy files into structured folders with AI.",
   manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body className={`${satoshi.className} text-[#203047]`}>
